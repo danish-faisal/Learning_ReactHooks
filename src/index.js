21e608afa2bba2bf1107b1527bdcb0ffdd646939
= <danishfaisal.wwe@gmail.com>
2022-01-23 19:52:49 +0530

    Sending internactions up component trees

--- src/index.js
+++ src/index.js
@@ -5,23 +5,31 @@ import { FaStar } from 'react-icons/fa';
 
 const createArray = (length) => [...Array(length)];
 
-function Star({ selected = false }) {
+function Star({ selected = false, onSelect }) {
   return (
-    <FaStar color={selected ? "red" : "grey"} />
+    <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />
   );
 }
 
 function StarRating({ totalStars = 5 }) {
-  return createArray(totalStars).map((n, i) => (
-    <Star key={i} />
-  ));
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {
+        createArray(totalStars).map((n, i) => (
+          <Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)} />
+        ))
+      }
+      <p>{selectedStars} of {totalStars}</p>
+    </>
+  );
 }
 
 function App() {
 
   return (
     <div className="App">
-      <StarRating totalStars={10} />
+      <StarRating totalStars={5} />
     </div>
   );
 }
